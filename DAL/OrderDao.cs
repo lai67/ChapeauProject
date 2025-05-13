8ff7e313167ac8dd3e1d01b3f379d5111b271748
AmazingBongo811 <aron.lakatos123@gmail.com>
2025-05-13 16:04:13 +0200

    Refactor OrderDao and OrderItemDao to use OpenConnection and CreateCommand for database operations

--- DAL/OrderDao.cs
+++ DAL/OrderDao.cs
@@ -4,10 +4,8 @@ using System.Data.SqlClient;
 
 namespace DAL
 {
-    internal class OrderDao
+    internal class OrderDao : BaseDao
     {
-        private string connectionString = "your_connection_string_here";
-
         public enum OrderStatus
         {
             Running,
@@ -21,26 +19,14 @@ namespace DAL
             List<Order> orders = new List<Order>();
             string query = "SELECT * FROM [Order]";
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = OpenConnection())
             {
-                SqlCommand command = new SqlCommand(query, connection);
-                connection.Open();
+                SqlCommand command = CreateCommand(connection, query);
                 SqlDataReader reader = command.ExecuteReader();
 
                 while (reader.Read())
                 {
-                    Order order = new Order
-                    {
-                        Id = reader.GetInt32(0),
-                        OrderTime = reader.GetDateTime(1),
-                        PreparationTime = reader.GetDateTime(2),
-                        Status = reader.GetString(3),
-                        EmployeeId = reader.GetInt32(4),
-                        BillId = reader.GetInt32(5),
-                        PreparationLocation = reader.GetString(6),
-                        TableId = reader.GetInt32(7)
-                    };
-                    orders.Add(order);
+                    orders.Add(MapOrder(reader));
                 }
             }
 
@@ -54,20 +40,20 @@ namespace DAL
                 throw new ArgumentException("Invalid order status.");
             }
 
-            string query = "INSERT INTO [Order] (order_time, preparation_time, status, employee_id, bill_id, preparation_location, table_id) VALUES (@orderTime, @prepTime, @status, @employeeId, @billId, @prepLocation, @tableId)";
+            string query = "INSERT INTO [Order] (order_time, preparation_time, status, employee_id, bill_id, preparation_location, table_id) " +
+                           "VALUES (@orderTime, @prepTime, @status, @employeeId, @billId, @prepLocation, @tableId)";
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = OpenConnection())
             {
-                SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@orderTime", order.OrderTime);
-                command.Parameters.AddWithValue("@prepTime", order.PreparationTime);
-                command.Parameters.AddWithValue("@status", order.Status);
-                command.Parameters.AddWithValue("@employeeId", order.EmployeeId);
-                command.Parameters.AddWithValue("@billId", order.BillId);
-                command.Parameters.AddWithValue("@prepLocation", order.PreparationLocation);
-                command.Parameters.AddWithValue("@tableId", order.TableId);
+                SqlCommand command = CreateCommand(connection, query,
+                    new SqlParameter("@orderTime", order.OrderTime),
+                    new SqlParameter("@prepTime", order.PreparationTime),
+                    new SqlParameter("@status", order.Status),
+                    new SqlParameter("@employeeId", order.EmployeeId),
+                    new SqlParameter("@billId", order.BillId),
+                    new SqlParameter("@prepLocation", order.PreparationLocation),
+                    new SqlParameter("@tableId", order.TableId));
 
-                connection.Open();
                 command.ExecuteNonQuery();
             }
         }
@@ -81,13 +67,12 @@ namespace DAL
 
             string query = "UPDATE [Order] SET status = @newStatus WHERE id = @orderId";
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = OpenConnection())
             {
-                SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@newStatus", newStatus);
-                command.Parameters.AddWithValue("@orderId", orderId);
+                SqlCommand command = CreateCommand(connection, query,
+                    new SqlParameter("@newStatus", newStatus),
+                    new SqlParameter("@orderId", orderId));
 
-                connection.Open();
                 command.ExecuteNonQuery();
             }
         }
@@ -97,31 +82,34 @@ namespace DAL
             List<Order> runningOrders = new List<Order>();
             string query = "SELECT * FROM [Order] WHERE status = @status";
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = OpenConnection())
             {
-                SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@status", OrderStatus.Running.ToString());
-                connection.Open();
+                SqlCommand command = CreateCommand(connection, query,
+                    new SqlParameter("@status", OrderStatus.Running.ToString()));
                 SqlDataReader reader = command.ExecuteReader();
 
                 while (reader.Read())
                 {
-                    Order order = new Order
-                    {
-                        Id = reader.GetInt32(0),
-                        OrderTime = reader.GetDateTime(1),
-                        PreparationTime = reader.GetDateTime(2),
-                        Status = reader.GetString(3),
-                        EmployeeId = reader.GetInt32(4),
-                        BillId = reader.GetInt32(5),
-                        PreparationLocation = reader.GetString(6),
-                        TableId = reader.GetInt32(7)
-                    };
-                    runningOrders.Add(order);
+                    runningOrders.Add(MapOrder(reader));
                 }
             }
 
             return runningOrders;
+        }
+
+        private Order MapOrder(SqlDataReader reader)
+        {
+            return new Order
+            {
+                Id = reader.GetInt32(0),
+                OrderTime = reader.GetDateTime(1),
+                PreparationTime = reader.GetDateTime(2),
+                Status = reader.GetString(3),
+                EmployeeId = reader.GetInt32(4),
+                BillId = reader.GetInt32(5),
+                PreparationLocation = reader.GetString(6),
+                TableId = reader.GetInt32(7)
+            };
         }
     }
 }
